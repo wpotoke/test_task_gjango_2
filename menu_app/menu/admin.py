a3a566a431443c20a1a8a1f39c92e4414e4f7a55
moroz <morozovdaniel713@gmail.com>
2025-05-14 16:56:18 +0300

    feat(menu): register mainmenu model

--- menu_app/menu/admin.py
+++ menu_app/menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from menu.models import MainMenu
 
 # Register your models here.
+@admin.register(MainMenu)
+class MainMenuAdmin(admin.ModelAdmin):
+    list_display = ["name"]
