7e5919884afbef317f99fc7e2c390c9298352911
moroz <morozovdaniel713@gmail.com>
2025-05-14 16:55:59 +0300

    feat(menu): add mainmenu model

--- menu_app/menu/models.py
+++ menu_app/menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MainMenu(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Главное меню")
+
+    class Meta:
+        db_table = "main_menu"
+        verbose_name = "Гланое меню"
+        verbose_name_plural = "Главные меню"
+
+    def __str__(self):
+        return f"{self.name}"
