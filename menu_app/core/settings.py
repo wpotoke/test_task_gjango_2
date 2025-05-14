5cbf5c53ff67c29ec0e00cecf56b72de81b17bdf
moroz <morozovdaniel713@gmail.com>
2025-05-14 16:18:20 +0300

    fix(core): correct name core

--- menu_app/core/settings.py
+++ menu_app/core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'menu.apps.config',
+    'menu.apps.MenuConfig',
 ]
 
 MIDDLEWARE = [
