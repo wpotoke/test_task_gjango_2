49e5d0cefe904e6ebadca4e027841ce6992c075e
moroz <morozovdaniel713@gmail.com>
2025-05-14 20:19:24 +0300

    feat(menu): add Sub menu, create and apply migrations

--- menu_app/menu/models.py
+++ menu_app/menu/models.py
@@ -11,3 +11,18 @@ class MainMenu(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class SubMenu(models.Model):
+
+    main_menu = models.ForeignKey(MainMenu, on_delete=models.PROTECT, related_name="sub", verbose_name="Главное меню")
+    name = models.CharField(max_length=100, unique=True, verbose_name="Вложенное меню")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="URL")
+
+    class Meta:
+        db_table = "sub_menu"
+        verbose_name = "Вложенное меню"
+        verbose_name_plural = "Вложенные меню"
+
+    def __str__(self):
+        return f"{self.name}"
