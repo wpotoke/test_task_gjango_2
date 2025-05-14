a504dca9d51a542625066b3d17bbd1cbb980e515
moroz <morozovdaniel713@gmail.com>
2025-05-14 20:21:11 +0300

    feat(menu): register submenu

--- menu_app/menu/admin.py
+++ menu_app/menu/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from menu.models import MainMenu
+from menu.models import MainMenu, SubMenu
 
 # Register your models here.
 @admin.register(MainMenu)
 class MainMenuAdmin(admin.ModelAdmin):
     list_display = ["name"]
+
+@admin.register(SubMenu)
+class SubMenuAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    prepopulated_fields = {"slug": ("name", )}
